8b1fde508b65060135d563b754056dd8122cc0a6
DDTChen <ddttchen@gmail.com>
2016-02-23 10:39:18 +0800

    改用webpack-hot-middleware

--- Module-Example/build/ddt.js
+++ Module-Example/build/ddt.js
@@ -1,87 +1,81 @@
 /******/ (function(modules) { // webpackBootstrap
 /******/ 	// The module cache
 /******/ 	var installedModules = {};
-/******/
+
 /******/ 	// The require function
 /******/ 	function __webpack_require__(moduleId) {
-/******/
+
 /******/ 		// Check if module is in cache
 /******/ 		if(installedModules[moduleId])
 /******/ 			return installedModules[moduleId].exports;
-/******/
+
 /******/ 		// Create a new module (and put it into the cache)
 /******/ 		var module = installedModules[moduleId] = {
 /******/ 			exports: {},
 /******/ 			id: moduleId,
 /******/ 			loaded: false
 /******/ 		};
-/******/
+
 /******/ 		// Execute the module function
 /******/ 		modules[moduleId].call(module.exports, module, module.exports, __webpack_require__);
-/******/
+
 /******/ 		// Flag the module as loaded
 /******/ 		module.loaded = true;
-/******/
+
 /******/ 		// Return the exports of the module
 /******/ 		return module.exports;
 /******/ 	}
-/******/
-/******/
+
+
 /******/ 	// expose the modules object (__webpack_modules__)
 /******/ 	__webpack_require__.m = modules;
-/******/
+
 /******/ 	// expose the module cache
 /******/ 	__webpack_require__.c = installedModules;
-/******/
+
 /******/ 	// __webpack_public_path__
 /******/ 	__webpack_require__.p = "";
-/******/
+
 /******/ 	// Load entry module and return exports
 /******/ 	return __webpack_require__(0);
 /******/ })
 /************************************************************************/
 /******/ ([
 /* 0 */
-/*!********************!*\
-  !*** ./src/app.js ***!
-  \********************/
 /***/ function(module, exports, __webpack_require__) {
 
 	'use strict';
-	
-	var _export_function = __webpack_require__(/*! ./export_function.js */ 1);
-	
+
+	var _export_function = __webpack_require__(1);
+
 	var _export_function2 = _interopRequireDefault(_export_function);
-	
-	var _export_class = __webpack_require__(/*! ./export_class.js */ 2);
-	
+
+	var _export_class = __webpack_require__(2);
+
 	var _export_class2 = _interopRequireDefault(_export_class);
-	
-	var _export_var = __webpack_require__(/*! ./export_var.js */ 3);
-	
+
+	var _export_var = __webpack_require__(3);
+
 	var Math = _interopRequireWildcard(_export_var);
-	
+
 	function _interopRequireWildcard(obj) { if (obj && obj.__esModule) { return obj; } else { var newObj = {}; if (obj != null) { for (var key in obj) { if (Object.prototype.hasOwnProperty.call(obj, key)) newObj[key] = obj[key]; } } newObj.default = obj; return newObj; } }
-	
+
 	function _interopRequireDefault(obj) { return obj && obj.__esModule ? obj : { default: obj }; }
-	
+
 	console.log((0, _export_function2.default)(1, 2));
 	console.log((0, _export_var.sub)(2, 1));
 	console.log('-----');
 	console.log((0, _export_var.sub)(Math.PI, 1));
-	
+
 	var abc = new _export_class2.default();
 	abc.print();
 
 /***/ },
 /* 1 */
-/*!********************************!*\
-  !*** ./src/export_function.js ***!
-  \********************************/
 /***/ function(module, exports) {
 
 	"use strict";
-	
+
 	Object.defineProperty(exports, "__esModule", {
 	    value: true
 	});
@@ -92,57 +86,50 @@
 
 /***/ },
 /* 2 */
-/*!*****************************!*\
-  !*** ./src/export_class.js ***!
-  \*****************************/
 /***/ function(module, exports) {
 
 	'use strict';
-	
+
 	Object.defineProperty(exports, "__esModule", {
 	    value: true
 	});
-	
+
 	var _createClass = function () { function defineProperties(target, props) { for (var i = 0; i < props.length; i++) { var descriptor = props[i]; descriptor.enumerable = descriptor.enumerable || false; descriptor.configurable = true; if ("value" in descriptor) descriptor.writable = true; Object.defineProperty(target, descriptor.key, descriptor); } } return function (Constructor, protoProps, staticProps) { if (protoProps) defineProperties(Constructor.prototype, protoProps); if (staticProps) defineProperties(Constructor, staticProps); return Constructor; }; }();
-	
+
 	function _classCallCheck(instance, Constructor) { if (!(instance instanceof Constructor)) { throw new TypeError("Cannot call a class as a function"); } }
-	
+
 	var utility = function () {
 	    function utility() {
 	        _classCallCheck(this, utility);
 	    }
-	
+
 	    _createClass(utility, [{
 	        key: 'print',
 	        value: function print() {
 	            console.log('utility printed');
 	        }
 	    }]);
-	
+
 	    return utility;
 	}();
-	
+
 	exports.default = utility;
 
 /***/ },
 /* 3 */
-/*!***************************!*\
-  !*** ./src/export_var.js ***!
-  \***************************/
 /***/ function(module, exports) {
 
 	"use strict";
-	
+
 	Object.defineProperty(exports, "__esModule", {
 	    value: true
 	});
 	exports.sub = sub;
 	var PI = exports.PI = 3.14;
-	
+
 	function sub(x, y) {
 	    return x - y;
 	};
 
 /***/ }
 /******/ ]);
-//# sourceMappingURL=ddt.js.map
